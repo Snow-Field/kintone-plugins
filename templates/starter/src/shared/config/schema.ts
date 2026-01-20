dd44852c43d85e22be35e93dbe7998676c8f5af5
git-sys-reno <git-sys-reno@comture.com>
2026-01-20 11:10:16 +0900

    chore: テンプレートのスキーマを修正

--- templates/starter/src/shared/config/schema.ts
+++ templates/starter/src/shared/config/schema.ts
@@ -5,8 +5,8 @@ export const LATEST_PLUGIN_VERSION = 1;
 
 /** 基本スキーマ */
 export const PluginConfigSchema = z.object({
-  version: z.literal(LATEST_PLUGIN_VERSION).default(LATEST_PLUGIN_VERSION),
-  message: z.string().default('Hello kintone!'),
+  version: z.literal(LATEST_PLUGIN_VERSION),
+  message: z.string(),
 });
 
 /** 型定義 */
