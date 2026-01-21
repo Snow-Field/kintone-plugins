194b6a20aee0b91b4bdd3e6acdd9cd3b1c9ab0b7
git-sys-reno-com <git-sys-reno@comture.com>
2026-01-21 13:03:28 +0900

    chore: ラベル変更

--- apps/age-calculator/src/config/components/features/AdvancedSettings.tsx
+++ apps/age-calculator/src/config/components/features/AdvancedSettings.tsx
@@ -8,7 +8,7 @@ export const AdvancedSettings: FC = () => {
       <Text variant='description' last>
         レコード保存時に年齢計算を実行するか選択してください。
       </Text>
-      <FormSwitch name='advanced.isUpdateOnSave' label='レコードを保存する度に年齢を更新する' />
+      <FormSwitch name='advanced.isUpdateOnSave' label='レコード保存時に年齢を更新する' />
     </FormSection>
   );
 };
