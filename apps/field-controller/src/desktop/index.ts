553c053bd8375d6ade7009b7e76b4adb2f9bbc92
git-sys-reno-com <git-sys-reno@comture.com>
2026-01-22 10:58:53 +0900

    feat: イベントに実行処理を登録

--- apps/field-controller/src/desktop/index.ts
+++ apps/field-controller/src/desktop/index.ts
@@ -1,12 +1,22 @@
+import { restoreConfig } from '@/shared/config';
+import { executeDisable } from '@/shared/lib/disableExecutor';
+import { executeVisibility } from '@/shared/lib/visibilityExecutor';
 import { PluginLogger } from '@kintone-plugin/kintone-utils';
 
 const logger = new PluginLogger('Desktop');
 
-(function () {
-  'use strict';
-
-  kintone.events.on('app.record.index.show', (event) => {
-    logger.info('Hello kintone! Plugin is active.');
+kintone.events.on(
+  [
+    'app.record.index.edit.show',
+    'app.record.detail.show',
+    'app.record.create.show',
+    'app.record.edit.show',
+  ],
+  (event) => {
+    logger.info('Plugin is active.');
+    const pluginConfig = restoreConfig();
+    executeDisable(pluginConfig.disableRules, event);
+    executeVisibility(pluginConfig.visibilityRules, event);
     return event;
-  });
-})();
+  }
+);
--- apps/field-controller/src/mobile/index.ts
+++ apps/field-controller/src/mobile/index.ts
@@ -1,12 +1,17 @@
+import { restoreConfig } from '@/shared/config';
+import { executeDisable } from '@/shared/lib/disableExecutor';
+import { executeVisibility } from '@/shared/lib/visibilityExecutor';
 import { PluginLogger } from '@kintone-plugin/kintone-utils';
 
 const logger = new PluginLogger('Mobile');
 
-(function () {
-  'use strict';
-
-  kintone.events.on('mobile.app.record.index.show', (event) => {
-    logger.info('Hello kintone mobile! Plugin is active.');
+kintone.events.on(
+  ['mobile.app.record.detail.show', 'mobile.app.record.create.show', 'mobile.app.record.edit.show'],
+  (event) => {
+    logger.info('Plugin is active.');
+    const pluginConfig = restoreConfig();
+    executeDisable(pluginConfig.disableRules, event);
+    executeVisibility(pluginConfig.visibilityRules, event);
     return event;
-  });
-})();
+  }
+);
